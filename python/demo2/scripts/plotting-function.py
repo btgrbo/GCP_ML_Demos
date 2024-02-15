ea4b6b56d43fb01833858cd20ec16b5fab0687da
Oliver Nowak <oliver.nowak@btelligent.de>
2024-02-15 17:48:13 +0100

    changes in main.py, pipfile and requirements & adding the whitepaper to the repository

--- python/demo2/scripts/plotting-function.py
+++ python/demo2/scripts/plotting-function.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Loading the data set into the environment
-df = pd.read_csv(r"C:/Users/OliverNowakbtelligen/OneDrive - b.telligent group/Desktop/GCP ML Demo/train.csv")
+df = pd.read_csv(r"C:\Users\OliverNowakbtelligen\OneDrive - b.telligent group\Desktop\Tickets\GCP ML Demos\python\demo2\test\training.csv")
 
 # Get summary statistics for numerical columns
 summary_statistics = df.describe()
@@ -11,15 +11,15 @@ print(summary_statistics)
 
 # Create boxplot
 plt.figure(figsize=(10, 4))
-sns.boxplot(x=df['Purchase'])
-plt.title('Boxplot for Purchase')
+sns.boxplot(x=df['Age'])
+plt.title('Boxplot for Age')
 plt.show()
 
 # Creating histograms
 plt.figure(figsize=(10, 4))
-sns.histplot(df['Purchase'], bins=30, kde=True, color='skyblue')  # Adjust bins and other parameters as needed
-plt.title('Histogram for Purchase')
-plt.xlabel('Purchase')
+sns.histplot(df['Age'], bins=30, kde=True, color='skyblue')  # Adjust bins and other parameters as needed
+plt.title('Histogram for Age')
+plt.xlabel('Age')
 plt.ylabel('Frequency')
 plt.show()
 
@@ -49,5 +49,5 @@ def create_boxplots_and_histograms(df):
 # Assuming df is your pandas df
 # create_boxplots_and_histograms(df)
 
-create_boxplots_and_histograms(df)
+create_boxplots_and_histograms(df["Age"])
 
